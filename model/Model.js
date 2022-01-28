d7d84d48c5e44edb96943d9eca59bddd239bb787
JoshDesmond <JoshDesmond@users.noreply.github.com>
2022-01-27 22:59:59 -0500

    Add card replacement

--- model/Model.js
+++ model/Model.js
@@ -1,17 +1,11 @@
 
 function getName(index) {
+    console.log(index);
     return fetch(`http://localhost:3000/${index}`)
         .then((response) => response.json())
 }
 
 const NUM_CHAMPS = 157;
-/**
- * @returns {Number} an integer between 0 and NUM_CHAMPS-1
- */
-function getRandomChampionInt() {
-    return Math.floor((Math.random() * NUM_CHAMPS));
-}
-
 
 class Model {
 
@@ -21,16 +15,39 @@ class Model {
      */
     constructor(setChampionFunction) {
         this.setChampion = setChampionFunction;
+        /** @type {Set} Set of champion indices remaining */
+        this._activeChampionList = new Set();
+        this._currentIndex = 0; // TODO duplicate state
+        for (let x = 0; x < NUM_CHAMPS; x++) {
+            this._activeChampionList.add(x);
+        }
     }
 
 
+    /**
+     * @returns {Number} an integer between 0 and NUM_CHAMPS-1
+     */
+    _getNextChampionInt() {
+        const champsRemaining = this._activeChampionList.size;
+        const index = Math.floor((Math.random() * champsRemaining));
+        let i = 0;
+
+        for (let val of this._activeChampionList.keys()) {
+            if (i++ === index) {
+                this._currentIndex = val; // TODO kinda hacky
+                return val;
+            }
+        }
+    }
 
     nextCard() {
-        const newIndex = getRandomChampionInt();
-        const that = this;
+        this._activeChampionList.delete(this._currentIndex); // TODO temp
+        const newIndex = this._getNextChampionInt();
+        console.log(`newIndex: ${newIndex}`);
         getName(newIndex)
             .then((data) => {
                 this.setChampion({ index: newIndex, name: data.name, image: data.image });
+                console.log(this._activeChampionList);
             });
     }
 }
